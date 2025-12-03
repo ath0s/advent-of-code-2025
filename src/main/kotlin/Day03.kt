3795574305dad8ab5ad05622b56ec8d1c2e5a903
Daniel Bergholm <861482+ath0s@users.noreply.github.com>
2025-12-03 19:22:45 +0100

    Day03 - Part 2

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -1,16 +1,10 @@
+import kotlin.io.path.readLines
+
 class Day03 : Day {
-    override fun partOne(filename: String, verbose: Boolean): Any {
-        val banks = filename.asPath().parseMatrix()
+    override fun partOne(filename: String, verbose: Boolean): Long {
+        val banks = filename.asPath().readLines()
         val maxJoltage = banks.map { bank ->
-            bank.foldIndexed(0L) { index, acc, current ->
-                if (index < bank.lastIndex) {
-                    bank.drop(index + 1).fold(acc) { acc, next ->
-                        maxOf(acc, "$current$next".toLong())
-                    }
-                } else {
-                    acc
-                }
-            }
+            bank.maxJoltages(2).toLong()
         }
         if (verbose) {
             maxJoltage.forEach {
@@ -20,8 +14,35 @@ class Day03 : Day {
         return maxJoltage.sum()
     }
 
-    override fun partTwo(filename: String, verbose: Boolean): Any {
-        TODO("Not yet implemented")
+    override fun partTwo(filename: String, verbose: Boolean): Long {
+        val banks = filename.asPath().readLines()
+        val maxJoltage = banks.map { bank ->
+            bank.maxJoltages(12).toLong()
+        }
+        if (verbose) {
+            maxJoltage.forEach {
+                println(it)
+            }
+        }
+        return maxJoltage.sum()
+    }
+
+    private fun String.maxJoltages(numberOfBatteries: Int): String {
+        if (numberOfBatteries == 0) {
+            return ""
+        }
+        if (length == numberOfBatteries) {
+            return this
+        }
+        var maxValue = '0'
+        var indexOfFirstMax = -1
+        substring(0, length - numberOfBatteries + 1).forEachIndexed { index, bank ->
+            if (bank > maxValue) {
+                maxValue = bank
+                indexOfFirstMax = index
+            }
+        }
+        return maxValue.toString() + substring(indexOfFirstMax + 1).maxJoltages(numberOfBatteries - 1)
     }
 
     companion object : Day.Main("Day03.txt") {
