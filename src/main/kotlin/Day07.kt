eddf264d4a6277f15cf40700dd0b9a7918d4f6da
Daniel Bergholm <861482+ath0s@users.noreply.github.com>
2025-12-07 11:31:56 +0100

    Day07 - Part 2

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -1,4 +1,7 @@
+import Direction.EAST
 import Direction.SOUTH
+import Direction.WEST
+
 
 class Day07 : Day {
     override fun partOne(filename: String, verbose: Boolean): Int {
@@ -15,7 +18,7 @@ class Day07 : Day {
                 }
                 if (diagram[below] == '^') {
                     usedSplits += below
-                    listOf(below.move(Direction.WEST), below.move(Direction.EAST)).filter {
+                    listOf(below.move(WEST), below.move(EAST)).filter {
                         it in diagram && diagram[it] != '^'
                     }
                 } else {
@@ -33,8 +36,29 @@ class Day07 : Day {
         return usedSplits.count()
     }
 
-    override fun partTwo(filename: String, verbose: Boolean): Any {
-        TODO("Not yet implemented")
+    override fun partTwo(filename: String, verbose: Boolean): Long {
+        val diagram = filename.asPath().parseMatrix()
+        var splits = mapOf(diagram.find { it == 'S' }!! to 1L)
+        loop@ while (true) {
+            splits = buildMap {
+                splits.forEach { (coordinate, count) ->
+                    val below = coordinate.move(SOUTH)
+                    if (below !in diagram) {
+                        break@loop
+                    }
+                    if (diagram[below] == '^') {
+                        val left = below.move(WEST)
+                        this[left] = (this[below.move(WEST)] ?: 0) + count
+
+                        val right = below.move(EAST)
+                        this[right] = (this[right] ?: 0) + count
+                    } else {
+                        this[below] = (this[below] ?: 0) + count
+                    }
+                }
+            }
+        }
+        return splits.values.sum()
     }
 
     companion object : Day.Main("Day07.txt") {
